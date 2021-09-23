8fb7a713c58ef49746b61bf9c9975c28af736380
navyakc <navyakelapanda55@gmail.com>
2021-09-23 12:54:50 +0530

    jen

---
+++ src/test/java/com/comcast/createOrgTest/TC_02_ContactsOrgTest.java
@@ -0,0 +1,29 @@
+package com.comcast.createOrgTest;
+
+import org.testng.annotations.Test;
+
+public class TC_02_ContactsOrgTest {
+
+	
+	@Test(groups="Smoke")
+	public void createContactOrgTest()
+	{
+		System.out.println("Create contact org");
+	}
+	
+	
+	
+	@Test
+	public void createContactwithOrgIndustryTest()
+	{
+		System.out.println("Create contact with org");
+	}
+	
+	
+	@Test
+	public void deleteContactOrgTest()
+	{
+		System.out.println("delete contact org");
+	}
+	
+}
---
+++ src/test/java/com/comcast/createcontactsTest/TC_01_ContactsTest.java
@@ -0,0 +1,29 @@
+package com.comcast.createcontactsTest;
+
+import org.testng.annotations.Test;
+
+public class TC_01_ContactsTest {
+
+	
+	@Test(groups="Smoke")
+	public void createContactTest()
+	{
+		System.out.println("Create contact");
+	}
+	
+	
+	
+	@Test(groups="Smoke")
+	public void createContactwithOrgTest()
+	{
+		System.out.println("Create contact with org");
+	}
+	
+	
+	@Test
+	public void deleteContactTest()
+	{
+		System.out.println("delete contact");
+	}
+	
+}
